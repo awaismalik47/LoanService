68150cd03d458f7dbe5c542a925f87821e6b5041
awaismalik47 <awaiskhaliq77.in@gmail.com>
2024-03-28 22:47:22 +0500

    Database Connected

--- app.js
+++ app.js
@@ -3,14 +3,5 @@ const express = require( 'express' );
 const app=express();
 app.use(express.json());
 
-app.get('/api/v1/loanService',(req,res ) => {
-   res.status(200).send('Hello From Server..')
-})
 
-
-const port=3000;
-app.listen(port ,()=>{
-    console.log( `Running server on ${port} ` );
-});
-
-
+module.exports=app;
---
+++ server.js
@@ -0,0 +1,33 @@
+const app=require('./app');
+const dotenv=require('dotenv');
+const mongoose=require('mongoose');
+const PORT=5300;
+dotenv.config({path:'./config.env'});
+
+process.on('uncaughtException',err=>{
+  console.log("Error",err.name,err.message);
+  process.exit(1)
+})
+
+const DB=process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD)
+// console.log(process.env.PORT);
+mongoose.connect(DB,{
+    // these properties are basically depreciation warnings
+    useNewUrlParser:true,
+}).then(()=>{
+console.log('Database Connected SuccessFully');
+});
+// Server listening ....
+ const server= app.listen(PORT, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", PORT);
+});
+
+process.on('unhandledRejection',err=>{
+  console.log("error",err.name,err.message);
+  server.close(()=>{
+    process.exit(1)
+  })
+});
+
+
